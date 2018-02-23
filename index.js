0a7f67939ba771c93b25d9bd6f47a592bba201c4
Paul Sheraton <paul.sheraton@peopleconnect.us>
2018-02-23 13:50:33 -0800

    adding in dep in contentFor

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-bing-maps'
+  name: 'ember-bing-maps',
+  contentFor(type) {
+    if (type === 'body') {
+      return `<script type="text/javascript" src="https://www.bing.com/api/maps/mapcontrol"></script>`;
+    }
+  }
 };
