db9e23c6d03e887cbf12d58931704c52b016b0c9
Paul Sheraton <paul.sheraton@peopleconnect.us>
2019-02-05 17:35:06 -0800

    Adding README, updating some unused code and refactoring for cleaner usage BREAKING CHANGE: some contracts are broken around events and infoboxes

--- app/components/private-bing-map.js
+++ app/components/private-bing-map.js
@@ -13,8 +13,6 @@ export default Component.extend({
   handlers: [],
   infoBox: null,
   defaultOpts: {
-    zoom: 10,
-    padding: 20,
     birdseyeFallback: {
       type: 'aerial',
       zoom: 15,
@@ -22,28 +20,30 @@ export default Component.extend({
       navBarOrientation: 'vertical',
     },
     defaultCenter: { location: { latitude: '45.0', longitude: '-100.0' }, width: 20, height: 28 },
-    enableClickableLogo: false,
-    showMapTypeSelector: true,
-    showTrafficButton: false,
     // Note: this does nothing for minified/compact menu view in birdseye...
     // allowHidingLabelsOfRoad: false,
-    disableMapTypeSelectorMouseOver: true,
-    // Note; bing ignores this unless its adding MORE types like the canvasDark et al...
-    supportedMapTypes: [
-      //'road',
-      //'aerial',
-      //'streetside',
-      'birdseye'
-    ],
+    enableClickableLogo: false,
     // Note: vertical + minified + birdseye = NO MENU...
     // Please also note birdseyeFallback in case birdseye is not available
     mapType: 'road',
+    mapTypeHover: false,
     // also note that minified is not supported for birdseye - Dafuck bing, serz?
     // this means if you start in birdseye, exit to aerial you cant get back in too...fun
     navBarMode: 'minified',
     navBarOrientation: 'vertical',
+    padding: 20,
+    showMapTypeSelector: true,
+    showTrafficButton: false,
+    // Note; bing ignores this unless its adding MORE types like the canvasDark et al...
+    supportedMapTypes: [
+      'road',
+      'aerial',
+      'streetside',
+      'birdseye'
+    ],
+    zoom: 10
   },
-  events: {
+  eventHandlers: {
     pin: {
       //'click': (e, infoBox, map) => {},
       //'mousedown': (e, infoBox, map) => {},
@@ -72,40 +72,14 @@ export default Component.extend({
     this.removeMap();
   },
 
-  addCustomInfoBoxes: function (map, customInfoBoxes) {
-    customInfoBoxes = (customInfoBoxes || []).map(({ infoBoxTemplate, description, location }) => {
-      if (infoBoxTemplate && description && location) {
-        return {
-          content: new this.Microsoft.Maps.Infobox(new this.Microsoft.Maps.Location(location.latitude, location.longitude), {
-            htmlContent: infoBoxTemplate.replace('{description}', description)
-          })
-        }
-      }
-      return false;
-    }).filter(Boolean);
-    customInfoBoxes.forEach((customInfoBox) => {
-      customInfoBox.content.setMap(map);
-    });
-  },
-
   createMap: function () {
-    let Microsoft = this.get('Microsoft');
+    let Microsoft = get(this, 'Microsoft');
     let el = get(this, 'element');
     let opts = get(this, 'mapOptions');
 
     this.getMapTypeId(opts).then(options => {
       let map = new Microsoft.Maps.Map(el, options);
 
-      let infoBox = new Microsoft.Maps.Infobox(map.getCenter(), {
-        visible: false
-      });
-
-      this.set('infoBox', infoBox);
-
-      let customInfoBoxes = get(this, 'infoBoxes');
-      if (customInfoBoxes) {
-        this.addCustomInfoBoxes(map, customInfoBoxes);
-      }
 
       this.set('map', map);
       this.updateCenter();
@@ -113,69 +87,96 @@ export default Component.extend({
   },
 
   clearEntities: function () {
-    let map = get(this, 'map');
+    let Microsoft = get(this, 'Microsoft');
+
+    // Clear handlers
     let handlers = get(this, 'handlers');
-    let Microsoft = this.get('Microsoft');
     (handlers || []).forEach(Microsoft.Maps.Events.removeHandler);
     this.set('handlers', []);
+
+    // Clear infoBoxes
+    let infoBoxes = get(this, 'currentInfoBoxes');
+    (infoBoxes || []).forEach((ib) => ib.setMap(null));
+    this.set('currentInfoBoxes', []);
+
+    // Clear pins
+    let map = get(this, 'map');
     if (map) {
       map.entities.clear();
     }
   },
 
   // Useful for things like on-click re-center map, pop dialog box, others..
-  addPinEvents: function (pin) {
-    let {
-      handlers,
-      events,
-      infoBox,
-      map
-    } = getProperties(this, 'handlers', 'events', 'infoBox', 'map');
-    let pinEvents = getWithDefault(events, 'pin', {});
-    let Microsoft = this.get('Microsoft');
-
-    Object.keys(pinEvents).map((eventName) => {
-      handlers.push(Microsoft.Maps.Events.addHandler(
-        pin,
-        eventName,
-        (e) => pinEvents[eventName](e, infoBox, map)
-      ));
-    });
-  },
-
   updateCenter: function () {
     let {
       map,
-      mapOptions,
-      locations
-    } = getProperties(this, 'map', 'mapOptions', 'locations');
-    let Microsoft = this.get('Microsoft');
+      mapOptions
+    } = getProperties(this, 'map', 'mapOptions');
 
     if (map) {
       let { center, bounds, zoom, padding } = mapOptions;
       map.setView({ center, bounds, zoom, padding });
       this.clearEntities();
-      locations.forEach((location) => {
-        let pin = new Microsoft.Maps.Pushpin(location.loc, location.options);
-        pin.metadata = (location.options || {}).metadata || {};
-        map.entities.push(pin);
-        this.addPinEvents(pin);
-      });
+      this.addPins(map);
+      this.addInfoBoxes(map);
     }
+  },
+
+  addPins: function (map) {
+    let Microsoft = get(this, 'Microsoft');
+    let locations = getWithDefault(this, 'locations', []);
+    locations.forEach((location) => {
+      let pin = new Microsoft.Maps.Pushpin(location.loc, location.options);
+      pin.metadata = location.options.metadata || {};
+      map.entities.push(pin);
+      this.addPinEvents(pin);
+    });
+  },
+
+  addPinEvents: function (pin) {
+    let {
+      handlers,
+      eventHandlers = {}
+    } = getProperties(this, 'handlers', 'eventHandlers');
+    let pinEvents = getWithDefault(eventHandlers, 'pin', {});
+    let Microsoft = get(this, 'Microsoft');
+
+    Object.keys(pinEvents).map((eventName) => {
+      handlers.push(Microsoft.Maps.Events.addHandler(
+        pin,
+        eventName,
+        (e) => pinEvents[eventName](e, this),
+      ));
+    });
+  },
+
+  addInfoBoxes: function (map) {
+    let Microsoft = get(this, 'Microsoft');
+    let infoBoxes = getWithDefault(this, 'infoBoxes', []);
+    let currentInfoBoxes = getWithDefault(this, 'currentInfoBoxes', []);
+    infoBoxes.forEach(({ location, options = {} }) => {
+        if (options.point) {
+          options.point = new Microsoft.Maps.Point(options.point.x, options.point.y);
+        }
+        let infoBox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(location.latitude, location.longitude), options);
+        infoBox.setMap(map);
+        currentInfoBoxes.push(infoBox);
+    });
+    this.set('currentInfoBoxes', currentInfoBoxes);
   },
 
   removeMap: function () {
     this.clearEntities();
-    let map = this.get('map');
+    let map = get(this, 'map');
     if (map) {
       map.dispose();
     }
   },
 
   locations: computed('pins', function (pins) {
-    let Microsoft = this.get('Microsoft');
+    let Microsoft = get(this, 'Microsoft');
 
-    return (pins || []).map(({ latitude, longitude, options }) => {
+    return (pins || []).map(({ latitude, longitude, options = {} }) => {
       if (latitude && longitude) {
         return { loc: new Microsoft.Maps.Location(latitude, longitude), options }
       }
@@ -184,8 +185,8 @@ export default Component.extend({
   }),
 
   centerBounds: computed('locations', function(locations) {
+    let Microsoft = get(this, 'Microsoft');
     let bounds;
-    let Microsoft = this.get('Microsoft');
 
     if (locations.length) {
       bounds = Microsoft.Maps.LocationRect.fromLocations(locations.map(l => l.loc));
@@ -194,7 +195,7 @@ export default Component.extend({
   }),
 
   setCenterBasedOnLocations: function (opts, locations) {
-    let centerBounds = this.get('centerBounds');
+    let centerBounds = get(this, 'centerBounds');
 
     if (locations.length > 1) {
       opts.bounds = centerBounds;
@@ -213,14 +214,15 @@ export default Component.extend({
   },
 
   mapOptions: computed('options', 'defaultOpts', 'locations', function(options, defaultOpts, locations) {
+    let Microsoft = get(this, 'Microsoft');
     let opts = Object.assign({}, defaultOpts, options);
-    let Microsoft = this.get('Microsoft');
 
     // Microsoft-ize em all here -- these need documentation
     opts.bounds = new Microsoft.Maps.LocationRect(opts.defaultCenter.location, opts.defaultCenter.width, opts.defaultCenter.height);
     opts.supportedMapTypes = opts.supportedMapTypes.map(mType => Microsoft.Maps.MapTypeId[mType] || mType);
     opts.navigationBarMode = Microsoft.Maps.NavigationBarMode[opts.navBarMode];
     opts.navigationBarOrientation = Microsoft.Maps.NavigationBarOrientation[opts.navBarOrientation];
+    opts.disableMapTypeSelectorMouseOver = !opts.mapTypeHover
 
     this.setCenterBasedOnLocations(opts, locations);
 
@@ -228,7 +230,7 @@ export default Component.extend({
   }),
 
   getMapTypeId: function(options) {
-    let Microsoft = this.get('Microsoft');
+    let Microsoft = get(this, 'Microsoft');
     options.mapTypeId = Microsoft.Maps.MapTypeId[options.mapType];
     if (options.mapTypeId === Microsoft.Maps.MapTypeId.birdseye) {
       return this.checkBirdseye().then((isAvail) => {
@@ -246,9 +248,9 @@ export default Component.extend({
   },
 
   checkBirdseye: function() {
-    let location = this.get('centerBounds.center');
+    let Microsoft = get(this, 'Microsoft');
+    let location = get(this, 'centerBounds.center');
     let deferred = RSVP.defer();
-    let Microsoft = this.get('Microsoft');
 
     if (!location) {
       deferred.resolve(false);
